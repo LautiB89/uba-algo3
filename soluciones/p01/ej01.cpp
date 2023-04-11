19e426d359377f3e34be08136aa991b80a84bf6a
LautiB89 <lbagnasco@dc.uba.ar>
2023-04-11 17:23:52 -0300

    arreglo tabs

--- soluciones/p01/ej01.cpp
+++ soluciones/p01/ej01.cpp
@@ -7,26 +7,26 @@ using namespace std;
 
 // Ejercicio 1
 
-  bool validSol(vector<int> s, int k) { 
-    int sum = 0, n = s.size();
-    for(int i = 0; i < n; i++)
-      sum += s[i];
-    return (sum == k); 
-  }
+bool validSol(vector<int> s, int k) { 
+  int sum = 0, n = s.size();
+  for(int i = 0; i < n; i++)
+    sum += s[i];
+  return (sum == k); 
+}
 
-  void ss(vector<int> &s, vector<int> &partialSol, vector<vector<int>> &sol, int i, int &k) {
-    if (s.size() == i) {
-      if (validSol(partialSol, k)){
-        sol.push_back(partialSol);
-      }
-      return;
+void ss(vector<int> &s, vector<int> &partialSol, vector<vector<int>> &sol, int i, int &k) {
+  if (s.size() == i) {
+    if (validSol(partialSol, k)){
+      sol.push_back(partialSol);
     }
-    partialSol.push_back(s[i++]);
-    ss(s, partialSol, sol, i, k);
-    partialSol.pop_back();
-    ss(s, partialSol, sol, i, k);
     return;
-  };
+  }
+  partialSol.push_back(s[i++]);
+  ss(s, partialSol, sol, i, k);
+  partialSol.pop_back();
+  ss(s, partialSol, sol, i, k);
+  return;
+};
 
 vector<vector<int>> subset_sum(vector<int> input_set, int k) {
   // devuelve todos los subconjuntos de s que suman k
