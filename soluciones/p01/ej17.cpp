9c082e03d9e078ca25f7569a2a1b3ea1f43dd9fe
LautiB89 <lbagnasco@dc.uba.ar>
2024-08-26 22:46:53 -0300

    pusheo todo

--- soluciones/p01/ej17.cpp
+++ soluciones/p01/ej17.cpp
@@ -21,7 +21,7 @@ Cada actividad A tiene
 
 act(A, S, i)    <--- este hay que arrancarlo con i=0
     | i == n+1 = S
-    | i > 1 && A[i-1].f > A[i].s = act(A, S, i+1)
+    | i > 1 && S[i-1].f > A[i].s = act(A, S, i+1)
     | otherwise = mas grande entre act(A, S, i+1) y act(A, S U {A[i]}, i+1)
 
 act(A, S, i)    <--- este hay que arrancarlo con i=n
@@ -39,13 +39,9 @@ bool puedoAgregar(int final_t, int i) {
     return final_t <= s[i]; //defino s y t de forma que siempre pueda agregar i=1
 }
 
-int largo(int i) {
-    return t[i] - s[i];
-}
-
 int actDP(int i = 1, int ultHr = 1) { //devuelvo el tamaño del conj mas grande posible
-    if (i == n) //asi nos aseguramos el i > 1 de act(A, S, i)
-        return (int) puedoAgregar(ultHr, i);
+    if (i > n) //asi nos aseguramos el i > 1 de act(A, S, i)
+        return 0;
 
     if (memo[i][ultHr] == BOTTOM) {
         memo[i][ultHr] = puedoAgregar(ultHr, i) ?
@@ -62,8 +58,8 @@ int actDP(int i = 1, int ultHr = 1) { //devuelvo el tamaño del conj mas grande 
     }
 }
  */
+
 int main() {
-    cout << "aalertaaa!!! \a" << endl;
     actDP();
     showMat(memo);
     return 0;
